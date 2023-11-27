c28dcb5bddf34c7ad976d09438b4960b09cb4283
Tricoire Arnaud <139560809+Tricoire-Arnaud@users.noreply.github.com>
2023-11-27 14:00:24 +0100

    contact

--- Personal/forms/contact.php
+++ Personal/forms/contact.php
@@ -1,41 +1,43 @@
 <?php
-  /**
-   * Nécessite la bibliothèque "PHP Email Form"
-   * La bibliothèque "PHP Email Form" est disponible uniquement dans la version professionnelle du modèle.
-   * La bibliothèque doit être téléchargée à l'emplacement suivant : vendor/php-email-form/php-email-form.php
-   * Pour plus d'informations et d'aide : https://bootstrapmade.com/php-email-form/
-   */
+  // Remplacez 'VOTRE_CLE_API' par votre clé API Sendinblue
+  $api_key = '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';
 
-  // Remplacez contact@example.com par votre véritable adresse e-mail de réception
   $adresse_email_de_reception = 'Arnaud.tricoire@hotmail.com';
 
-  if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
-    include($php_email_form);
-  } else {
-    die('Impossible de charger la bibliothèque "PHP Email Form" !');
-  }
+  // Récupérez les données du formulaire
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-  $contact = new $php_email_form;
-  $contact->ajax = true;
-  
-  $contact->to = $adresse_email_de_reception;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Décommentez le code ci-dessous si vous souhaitez utiliser SMTP pour envoyer des e-mails. Vous devez entrer vos informations SMTP correctes.
-  /*
-  $contact->smtp = array(
-    'host' => 'exemple.com',
-    'username' => 'exemple',
-    'password' => 'mot_de_passe',
-    'port' => '587'
+  // Créez le tableau de données pour l'API Sendinblue
+  $data = array(
+    'to' => array(array('email' => $adresse_email_de_reception)),
+    'cc' => array(),
+    'bcc' => array(),
+    'replyTo' => array('email' => $email),
+    'subject' => $subject,
+    'text' => $message,
+    'html' => "<p>$message</p>",
+    'attachment' => array()
   );
-  */
 
-  $contact->add_message($_POST['name'], 'De');
-  $contact->add_message($_POST['email'], 'Email');
-  $contact->add_message($_POST['message'], 'Message', 10);
+  // Convertissez le tableau de données en JSON
+  $json_data = json_encode($data);
 
-  echo $contact->send();
+  // Utilisez cURL pour envoyer l'e-mail via l'API Sendinblue
+  $ch = curl_init('https://api.sendinblue.com/v3/smtp/email');
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'api-key: ' . $api_key));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+  // Exécutez la requête cURL
+  $result = curl_exec($ch);
+
+  // Fermez la session cURL
+  curl_close($ch);
+
+  // Affichez la réponse de l'API (pour le débogage)
+  echo $result;
 ?>
